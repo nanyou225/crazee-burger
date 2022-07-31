3226393d118648da375ca9b33ee7bb9754ba8e26
noman <nomanplacidenoman@gmail.com>
2022-07-31 13:28:27 +0100

    F02 feat Add LoginForm

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,26 +1,6 @@
-import { useState } from "react";
-import { createSearchParams, useNavigate } from "react-router-dom";
+import LoginForm from "../login/LoginForm";
 
 export default function LoginPage() {
-  //state
-  const [newName, setNewName] = useState("");
-  const navigate = useNavigate();
-
-  const handleSubmit = (event) => {
-    event.preventDefault(event);
-    setNewName("");
-    navigate({
-      pathname: "/OrderPage",
-      search: createSearchParams({
-        newName: newName,
-      }).toString(),
-    });
-  };
-
-  const handleChange = (event) => {
-    setNewName(event.target.value);
-  };
-
   //comportement
 
   //Affichage(render)
@@ -30,15 +10,7 @@ export default function LoginPage() {
       <hr />
       <h2>Bienvenu chez nous</h2>
       <h3>Connectez-vous</h3>
-      <form action="submit" onSubmit={handleSubmit}>
-        <input
-          value={newName}
-          onChange={handleChange}
-          type={"text"}
-          placeholder={"Entrez votre prénom..."}
-        />
-        <button>Accéder à mon espece</button>
-      </form>
+      <LoginForm />
     </div>
   );
 }
